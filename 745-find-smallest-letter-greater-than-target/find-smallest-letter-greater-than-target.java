772b556aad33d005aec620152816fabb59a7880b
Nikitha Reddy <nikithareddy1103@gmail.com>
2026-01-31 08:50:00 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (24.49%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
@@ -1,11 +1,25 @@
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
-        Arrays.sort(letters);
+       /* Arrays.sort(letters);
         for(int i=0;i<letters.length;i++){
             if(letters[i]>target){
                 return letters[i];
             }
         }
-        return letters[0];
+        return letters[0];*/
+        int left=0;
+        int right=letters.length-1;
+        char res=letters[0];
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(letters[mid]>target){
+                res=letters[mid];
+                right=mid-1;
+            }
+            else{
+                left=mid+1;
+            }
+        }
+        return res;
     }
 }

9b80905550e6a9dcd7d310cb6245a3870653b9ba
Nikitha Reddy <nikithareddy1103@gmail.com>
2026-02-04 22:55:10 +0530

    Time: 5 ms (6.90%) | Memory: 43.8 MB (25.42%) - LeetSync

--- 682-baseball-game/baseball-game.java
+++ 682-baseball-game/baseball-game.java
@@ -1,6 +1,6 @@
 class Solution {
     public int calPoints(String[] operations) {
-        Stack<Integer>st=new Stack<>();
+       /* Stack<Integer>st=new Stack<>();
         for(String str:operations){
             if(str.equals("+")){
                 int last=st.pop();
@@ -21,6 +21,27 @@ class Solution {
         int sum=0;
         for(int num:st){
             sum+=num;
+        }
+        return sum;*/
+        List<Integer>list=new ArrayList<>();
+        for(String s:operations){
+            int n=list.size();
+            if(s.equals("+")){
+                list.add(list.get(n-1)+list.get(n-2));
+            }
+            else if(s.equals("C")){
+                list.remove(n-1);
+            }
+            else if(s.equals("D")){
+                list.add(list.get(n-1)*2);
+            }
+            else{
+                list.add(Integer.parseInt(s));
+            }
+        }
+        int sum=0;
+        for(int ans:list){
+            sum+=ans;
         }
         return sum;
     }

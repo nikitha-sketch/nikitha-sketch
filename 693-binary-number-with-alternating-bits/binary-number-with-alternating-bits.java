f5c4988268a4fce5363888b25558a1044a3c5a8d
Nikitha Reddy <nikithareddy1103@gmail.com>
2026-02-18 09:45:58 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (88.31%) - LeetSync

--- 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.java
+++ 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.java
@@ -1,14 +1,15 @@
 class Solution {
     public boolean hasAlternatingBits(int n) {
-        StringBuilder str=new StringBuilder();
+        /*StringBuilder str=new StringBuilder();
         while(n>0){
             str.append(n%2);
             n=n/2;
         }
-        
         for(int i=0;i<str.length()-1;i++){
             if(str.charAt(i)==str.charAt(i+1))return false;
         }
-        return true;
+        return true;*/
+        int x=n^(n>>1);
+        return (x&(x+1))==0;
     }
 }

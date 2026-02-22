a96a38154b9ba4a9a2f806d26ce21da12675570b
Nikitha Reddy <nikithareddy1103@gmail.com>
2026-02-22 08:32:02 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (14.06%) - LeetSync

--- 899-binary-gap/binary-gap.java
+++ 899-binary-gap/binary-gap.java
@@ -1,6 +1,6 @@
 class Solution {
     public int binaryGap(int n) {
-        StringBuilder str=new StringBuilder();
+       /* StringBuilder str=new StringBuilder();
         while(n>0){
             str.append(n%2);
             n=n/2;
@@ -14,6 +14,20 @@ class Solution {
                 }
                 lastIndex=i;
             }
+        }
+        return maxGap;*/
+        int maxGap=0;
+        int lastIndex=-1;
+        int index=0;
+        while(n>0){
+            if((n&1)==1){
+                if(lastIndex!=-1){
+                    maxGap=Math.max(maxGap,index-lastIndex);
+                }
+                lastIndex=index;
+            }
+            n>>=1;
+            index++;
         }
         return maxGap;
     }
